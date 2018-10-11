31d0955d0c4e5a8ea3b44e0292eb70dcf81e8834
Trevor Sargent <trevor.a.sargent@gmail.com>
2018-10-11 12:51:11 -0700

    updates

--- autoload/aliases.sh
+++ autoload/aliases.sh
@@ -4,11 +4,8 @@ alias gh="hub browse" # open github for the current repo
 alias ls="ls -G" # colored ls output
 alias duh="du -sh" # quick file/directory size
 alias gitroot='cd $(git rev-parse --show-cdup)' # quickly cd to the repo root
-alias mm="middleman"
 alias flushdns="sudo discoveryutil udnsflushcaches"
 alias be="bundle exec"
-alias xkcdpass="~/.dotfiles/scripts/xkcdpass.rb"
-alias rezsh="reset && source ~/.zshrc"
 # clear AND erase scrollback in tmux, Terminal.app, and iTerm2
 alias cls="clear && printf '\e[3J' && printf '\e]50;ClearScrollback\a'"
 alias chat="open ~/.dotfiles/resources/chat.html"
@@ -16,11 +13,9 @@ alias 🚀="git push"
 alias 👊="git push -f"
 alias 🛁="git pull --rebase upstream master"
 alias oops="fuck"
-export EDITOR=subl
+export EDITOR=code
 alias nodesucks="nvm use && rm -rf node_modules && npm i"
 alias disapprove="echo -n ಠ_ಠ | pbcopy"
-alias sl="sublimate"
-
 alias weather="ansiweather -l 5746545"
 alias backtalk="echo fuck you"
 alias zoom="speedtest-cli"
@@ -28,3 +23,4 @@ alias mc="java -Xmx1024M -Xms1024M -jar minecraft_server.1.12.1.jar nogui"
 
 alias "jj"="bundle exec jekyll serve -w"
 alias "jjb"="bundle exec jekyll build"
+alias "f"="open ."
